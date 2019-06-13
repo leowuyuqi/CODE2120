52f89776855299eb4b82876225b87cb1d26c5971
leowuyuqi <leowuyuqi@users.noreply.github.com>
2019-06-13 13:18:22 +1000

    second class

--- mysite/example/views.py
+++ mysite/example/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 import json
 from .models import *
+import os, sys
 
 # Create your views here.
 
@@ -20,12 +21,35 @@ def example_get(request, var_a, var_b):
 		return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
 
 @csrf_exempt
-def example_post(request):
+def fib(request):
+	jsob = {"startNumber":0,"length":10}
 	log = []
 	if request.method == "POST":
 		try:
 			data = request.POST["data"]
-			return JsonResponse({"log":log})
+			received = json.loads(data)
+			jsob.update(received)
+
+
+
+			startNumber = int(jsob["startNumber"])
+			length      = int(jsob["length"])
+			loop        = range(length)
+
+			numarray    = []
+
+			fibno = startNumber
+			addno = 1
+
+			for l in loop:
+				numarray.append(fibno)
+				fibno = fibno+addno
+				addno = fibno-addno
+
+			
+
+			return JsonResponse({"fib":numarray})
+		
 		except Exception as e:
 			exc_type, exc_obj, exc_tb = sys.exc_info()
 			other = sys.exc_info()[0].__name__
